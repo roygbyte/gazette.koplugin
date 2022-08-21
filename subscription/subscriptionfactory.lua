fc50b5c887b323dbdadc86dca8b12c1678eda7c9
Scarlett <social@scarlettmcallister.com>
2022-08-21 20:03:12 -0300

    Add feed constructor to subscription factory

--- subscription/subscriptionfactory.lua
+++ subscription/subscriptionfactory.lua
@@ -1,3 +1,6 @@
+local FeedSubscription = require("subscription/type/feed")
+local Feed = require("feed/feed")
+
 local SubscriptionFactory = {
 
 }
@@ -5,5 +8,18 @@ local SubscriptionFactory = {
 SubscriptionFactory.SUBSCRIPTION_TYPES = {
    feed = "feed"
 }
+
+function SubscriptionFactory:makeFeed(configuration)
+   -- Do I need to assign the values... or can I just pass the configuration table?
+   return FeedSubscription:new{
+      url = configuration.url,
+      limit = configuration.limit,
+      download_full_article = configuration.download_full_article,
+      include_images = configuration.include_images,
+      enabled_filter = configuration.enabled_filter,
+      filter_element = configuration.filter_element,
+      feed = Feed:new(configuration.feed) or nil
+   }
+end
 
 return SubscriptionFactory
