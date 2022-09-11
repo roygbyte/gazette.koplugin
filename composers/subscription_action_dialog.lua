a64a5113be5c17650d061ece78b5a22cb8225f9c
Scarlett <social@scarlettmcallister.com>
2022-09-10 22:01:31 -0300

    Add subscription action dialog

---
+++ composers/subscription_action_dialog.lua
@@ -0,0 +1,39 @@
+local ButtonDialog = require("ui/widget/buttondialog")
+local UIManager = require("ui/uimanager")
+
+local GazetteMessages = require("gazettemessages")
+local ConfigureSubscription = require("composers/configure_subscription")
+
+local SubscriptionActionDialog = {}
+
+function SubscriptionActionDialog:show(subscription, on_close_callback)
+   local button_dialog -- Declare variable here so it can be passed into ButtonDialog
+   button_dialog = ButtonDialog:new{
+      buttons = {
+         {
+            {
+               text = GazetteMessages.SUBSCRIPTION_ACTION_DIALOG_EDIT,
+               callback = function()
+                  UIManager:close(button_dialog)
+                  ConfigureSubscription:editFeed(subscription, function()
+                        on_close_callback()
+                  end)
+               end,
+            },
+         },
+         {
+            {
+               text = GazetteMessages.SUBSCRIPTION_ACTION_DIALOG_DELETE,
+               callback = function()
+                  UIManager:close(button_dialog)
+                  subscription:delete()
+                  on_close_callback()
+               end,
+            },
+         },
+      }
+   }
+   UIManager:show(button_dialog)
+end
+
+return SubscriptionActionDialog
