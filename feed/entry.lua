5574ed633cec4dfd57960069f3606f4e7ad8af4e
Scarlett <social@scarlettmcallister.com>
2022-09-11 21:16:42 -0300

    Add helper method to return ID

--- feed/entry.lua
+++ feed/entry.lua
@@ -25,6 +25,10 @@ function Entry:getPublished()
    return self.pubDate or self.published
 end
 
+function Entry:getId()
+    return self:getPermalink()
+end
+
 function Entry:getPermalink()
    -- ID must preceed link, since Atom entries have
    -- both id and link attributes.
